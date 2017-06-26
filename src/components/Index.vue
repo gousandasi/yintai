3e6b806ad187d4b6da75f480962b194e371a86b0
gousandasi <18280391311@163.com>
2017-06-26 11:31:38 +0800

    frist time commit

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+   <h1>这就是我</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
